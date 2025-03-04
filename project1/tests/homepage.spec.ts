77b5ec5bbe4b51060b8ef1c12cc1cfaa192a3c4f
jcastillo <jayson.castillo@bayviewtechnology.com>
2025-03-04 11:48:21 +0800

    Updated the script

--- project1/tests/homepage.spec.ts
+++ project1/tests/homepage.spec.ts
@@ -2,5 +2,5 @@ import { test, expect } from '@playwright/test';
 
 test('Check facebook home page', async ({ page }) => {
     await page.goto('https://facebook.com');
-    await expect(page).toHaveTitle(/facebook/);
+    await expect(page).toHaveTitle(/Facebook/);
 })
--- project2/tests/open-google.spec.ts
+++ project2/tests/open-google.spec.ts
@@ -2,5 +2,5 @@ import { test, expect } from '@playwright/test';
 
 test('Check google home page', async ({ page }) => {
     await page.goto('https://google.com');
-    await expect(page).toHaveTitle(/google/);
+    await expect(page).toHaveTitle(/Google/);
 })
